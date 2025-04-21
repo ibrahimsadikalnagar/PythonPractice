4130f68fc29fcf3bd4d37a79e4187303d8e08d09
Ibrahim <alnajjar.ibrahim@gmail.com>
2025-04-21 08:27:37 +0200

    Oefenen fandamentals python only to get used with the python syntax

--- PythonApplication1/DealingWithConditions/DealingWithConditions.py
+++ PythonApplication1/DealingWithConditions/DealingWithConditions.py
@@ -21,15 +21,60 @@
 #     print('You are wrong'
 #            'you have to try again')
 
+# import random
+# import PythonApplication1
+
+
+# GenerateRandam = random.randint(1,5)
+# Counter = 0 ;
+
+# while(GenerateRandam != 5):
+#     Counter += 1;
+#     print(f"Computer choose not 5 it choose {GenerateRandam}")
+#     GenerateRandam = random.randint(1,5)
+# print(f"Computer try {Counter} times until it reach 5 ")
+
+# import random
+# friends = random.choice(["Ibrahim" , "Sal" , "Mom" , "Yossif" , "AboNadeem"])
+# # print(friends)
+
+# Random_index = random.randint(a:0 ,b: 5)
+# print(friends[Random_index])
+
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+# Dirty_food = [fruits , vegetables]
+# print(Dirty_food[1][1])
+
+# small apps for rock paper scissors 
 import random
-import PythonApplication1
 
 
-GenerateRandam = random.randint(1,5)
-Counter = 0 ;
+# print("choice sccers choice 1 , peper choice 2 , Rock choice 3")
+# user_Count = 0 ; 
+# Computer_Count = 0 ; 
 
-while(GenerateRandam != 5):
-    Counter += 1;
-    print(f"Computer choose not 5 it choose {GenerateRandam}")
-    GenerateRandam = random.randint(1,5)
-print(f"Computer try {Counter} times until it reach 5 ")
+# while user_Count != 3 or Computer_Count != 3:
+#    user_choice = input("Choice S , R , P ")
+#    Computer_choice = random.choice(["S" , "R" , "P"])
+#    if (user_choice == "S" and Computer_choice == "P") or\
+#    (user_choice == "R" and Computer_choice == "S" ) or\
+#   (user_choice == "P" and Computer_choice == "R"):
+#     print("User Win")
+#     user_Count += 1; 
+#    else :
+#     print("Computer Win")
+#     Computer_Count += 1
+
+# if user_Count == 3 :
+#     print ("\n\nGreate you win ")
+# else:
+#     print("\n\nComputer win")
+
+
+
+
+
+
+
